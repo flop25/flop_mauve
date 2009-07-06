8d9cc1b9cdeb2eaaa57483299634a12338e5328a
flop25 <flop25@piwigo.org>
2009-07-06 09:43:59 +0000

    git-svn-id: http://piwigo.org/svn/extensions/flop_mauve@3530 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themeconf.inc.php
@@ -0,0 +1,21 @@
+<?php
+$themeconf = array(
+  'template' => 'yoga',
+  'theme' => 'flop_mauve',
+  'icon_dir' => 'template/yoga/icon',
+  'admin_icon_dir' => 'template/yoga/icon/admin',
+  'mime_icon_dir' => 'template/yoga/icon/mimetypes/',
+  'local_head' => '<!--[if IE]><style>
+#content div.thumbnailCategory span {
+    display: inline-block;
+    height: 100%;
+}
+</style><![endif]-->
+');
+
+$conf['level_separator'] = ' &raquo; ';
+$conf['paginate_pages_around'] = 7;
+
+
+
+?>
