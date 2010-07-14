2118717d5e5b8fd1549121c0916c1c72c5fdda08
flop25 <flop25@piwigo.org>
2010-07-14 15:04:55 +0000

    git-svn-id: http://piwigo.org/svn/extensions/flop_mauve@6690 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ current/themeconf.inc.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Theme Name: flop_mauve
+Version: 3.1.0
+Description: A mauve theme derived from the dark theme of piwigo
+Theme URI: http://fr.piwigo.org/ext/extension_view.php?eid=385
+Author: flop25
+Author URI: http://www.planete-flop.fr
+*/
+$themeconf = array(
+  'name' => 'flop_mauve',
+  'parent'        => 'default',
+  'icon_dir'      => 'themes/default/icon',
+  'mime_icon_dir' => 'themes/default/icon/mimetypes/',
+  'local_head' => '<!--[if IE]><style>
+#content div.thumbnailCategory span {
+    display: inline-block;
+    height: 100%;
+}
+</style><![endif]-->
+');
+
+$conf['level_separator'] = ' &raquo; ';
+$conf['paginate_pages_around'] = 7;
+
+
+
+?>
