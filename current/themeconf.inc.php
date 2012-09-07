27c707a9bf68dae3325877579b3eead9d9cc0c9d
flop25 <flop25@piwigo.org>
2012-09-07 17:51:23 +0000

    correction for pwg 2.4 : truncate name and description no background image for H3
    git-svn-id: http://piwigo.org/svn/extensions/flop_mauve@17795 68402e56-0260-453c-a942-63ccdbb3a9ee

--- current/themeconf.inc.php
+++ current/themeconf.inc.php
@@ -19,5 +19,22 @@ $conf['level_separator'] = ' &raquo; ';
 $conf['paginate_pages_around'] = 7;
 $conf['show_picture_name_on_title'] = false;
 $conf['show_thumbnail_caption'] = false;
+/** mainpage_categories.tpl **/
+add_event_handler('loc_end_index_category_thumbnails', 'flopmauve_cat');
+function flopmauve_cat($tpl_thumbnails_var)
+{
+    global $template;
+    $template->set_prefilter('index_category_thumbnails', 'flopmauve_cat_pre');
+		return $tpl_thumbnails_var;
+}
+function flopmauve_cat_pre($content, &$smarty)
+{
+  $search = 'NAME}</a>';
+  $replacement = 'NAME|truncate:38:" [...]"}</a>';
+  $content=str_replace($search, $replacement, $content);
+  $search = 'DESCRIPTION}</p>';
+  $replacement = 'DESCRIPTION|truncate:130:" [...]"}</p>';
+  return str_replace($search, $replacement, $content);
+}
 
 ?>
